63288d3e7a5836e0875ae50558cd934e78e33d1e
Paul Ramon <peramon98.18@gmail.com>
2023-06-27 12:02:42 -0500

    Viewing Characters Strings Variables and memory

--- FirstExampleAlura/src/Main.java
+++ FirstExampleAlura/src/Main.java
@@ -12,25 +12,49 @@ public class Main {
         double salary = 750.50;
         double increase = salary + 125;
 
-        System.out.println("Mi current  salary is " + increase);
-        System.out.println("Mi age in a decade will be " + age);
+        System.out.println("Mi current  salary is -> " + increase);
+        System.out.println("Mi age in a decade will be -> " + age);
 
         // Conversions - Cast
         double varOne = 130.78;
         int varTwo = (int) varOne;
-        System.out.println("Double type to integer conversion " + varTwo);
+        System.out.println("Double type to integer conversion -> " + varTwo);
 
         // Long
         long test = 1222222222222222222L;
-        System.out.println("Number too long " + test);
+        System.out.println("Number too long -> " + test);
 
         // Short
         short smallNumber = 13555;
-        System.out.println("Short number " + smallNumber);
+        System.out.println("Short number -> " + smallNumber);
 
         // Byte
         byte smallerNumber = 15;
-        System.out.println("Smaller number " + smallerNumber);
+        System.out.println("Smaller number -> " + smallerNumber);
+
+        // Characters
+        char character = 'a';
+        System.out.println("Character -> " + character);
+        character = 65;
+        System.out.println("Altered character -> " + character);
+        character = 65 + 1;
+        char secondCharacter = (char) (character +1);
+        System.out.println("Second character -> " + secondCharacter);
+
+        // String
+        String word = "Online Alura courses";
+        System.out.println("Word -> " + word);
+
+        // Variables and memory
+        int number_one = 5;
+        int number_two = 9;
+        System.out.println("Number Two -> " + number_two);
+
+        number_two = number_one;
+        System.out.println("Number Two -> " + number_two);
+
+        number_one = 88;
+        System.out.println("Number Two -> " + number_two);
 
     }
 }
