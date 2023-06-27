26d1be793c6d70a64dd057d7fa452d44cbb0b96d
Paul Ramon <peramon98.18@gmail.com>
2023-06-27 11:12:03 -0500

    Viewing numerical data types and conversions

--- FirstExampleAlura/src/Main.java
+++ FirstExampleAlura/src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Variable Types
+        // Int
+        int age = 25;
+        int decade = 10;
+        age += decade;
+
+        // Double
+        double salary = 750.50;
+        double increase = salary + 125;
+
+        System.out.println("Mi current  salary is " + increase);
+        System.out.println("Mi age in a decade will be " + age);
+
+        // Conversions - Cast
+        double varOne = 130.78;
+        int varTwo = (int) varOne;
+        System.out.println("Double type to integer conversion " + varTwo);
+
+        // Long
+        long test = 1222222222222222222L;
+        System.out.println("Number too long " + test);
+
+        // Short
+        short smallNumber = 13555;
+        System.out.println("Short number " + smallNumber);
+
+        // Byte
+        byte smallerNumber = 15;
+        System.out.println("Smaller number " + smallerNumber);
+
     }
 }
