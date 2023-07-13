3e02538a41fc788af58c8bb497b0d30d4ab93fc6
Paul Ramon <peramon98.18@gmail.com>
2023-07-12 20:22:30 -0500

    Working with cycle while

---
+++ JavaJREJDK/src/Cycle.java
@@ -0,0 +1,13 @@
+public class Cycle {
+    public static void main(String[] args) {
+        System.out.println("Cycles");
+        // Initialize Counter
+        int counter = 0;
+        // Cycle While
+        while (counter < 10){
+            System.out.println("Cycle " + counter);
+            // Increase counter
+            counter++;
+        }
+    }
+}
